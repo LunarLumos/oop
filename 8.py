a5fd0868950b6927b5053e2c445cd3717d5c3051
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2026-04-17 11:32:19 +0600

    Add Animal, Dog, and Cat classes with speak methods
    Implement Animal, Dog, and Cat classes with speak method.

---
+++ 8.py
@@ -0,0 +1,19 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    
+    def speak(self):
+        pass
+
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} says Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return f"{self.name} says Meow!"
+
+d = Dog("Aadil's Pet")
+c = Cat("Miwo's Pet")
+print(d.speak())
+print(c.speak())
