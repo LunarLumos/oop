6ebc4e9c71ae32835a740bd7647eaeae47c75e39
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2026-04-17 11:38:37 +0600

    Implement Multiplier class for scaling numbers

---
+++ 27.py
@@ -0,0 +1,11 @@
+class Multiplier:
+    def __init__(self, factor):
+        self.factor = factor
+    
+    def __call__(self, x):
+        return x * self.factor
+
+double = Multiplier(2)
+print(double(5))
+triple = Multiplier(3)
+print(triple(5))
